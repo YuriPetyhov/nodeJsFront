b14d31959ee282ff712a068e1fbb1aeb7867d1d7
Yury Petukhou <mr.yra19911103@gmail.com>
2019-12-11 17:34:54 +0300

    Add provider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import LoginForm from "./components/loginForm";
 import { BrowserRouter } from 'react-router-dom';
 import Routers from './routers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text])
+    default:
+      return state
+  }
+}
+
+const store = createStore(todos, ['Use Redux'])
 function App() {
   return (
+    <Provider store={store}>
     <BrowserRouter>
        <Routers />
     </BrowserRouter>
+    </Provider>
   );
 }
 
--- my-app/src/components/registr.jsx
+++ my-app/src/components/registr.jsx
@@ -1,5 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Lock from '@material-ui/icons/Lock';
+import axios from 'axios'
 
-export default () => (
-  <div>registration</div>
-)
+const useStyles = makeStyles(theme => ({
+  login: {
+    display: 'block'
+  },
+  loginForm: {
+    width: '250px',
+    margin: '50px auto'
+  },
+  regNav: {
+    width: "100px",
+    display: "flex",
+    justifyContent: "space-between"
+  }
+}));
+
+const LoginForm = ( props ) => {
+
+  const [form, setForm] = useState({
+    login: '',
+    password: ''
+  })
+
+  const [err, setErr] = useState(false)
+
+  const handleSetForm = (e) => {
+
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+  const submitForm = (e) => {
+    axios.post('http://localhost:3100/signup', {...form})
+      .then(res => props.history.push('/'))
+      .catch(error => {
+        setErr(error.response.data.err)
+       console.log(error.response)
+      })
+
+      e.preventDefault()
+  }
+
+
+  const classes = useStyles();
+  return ( <form onSubmit={submitForm} className={classes.loginForm}>
+    <FormControl className={classes.login}>
+      <InputLabel htmlFor="login">Log in </InputLabel>
+      <Input
+        onChange={handleSetForm}
+        id="login"
+        name='login'
+        startAdornment={
+          <InputAdornment position="start">
+            <AccountCircle/>
+          </InputAdornment>
+        }
+      />
+      {err && <span> {err} </span>}
+    </FormControl>
+    <FormControl className={classes.margin}>
+      <InputLabel htmlFor="password">Passsword </InputLabel>
+      <Input
+        onChange={handleSetForm}
+        id='password'
+        name='password'
+        startAdornment={
+          <InputAdornment position="start">
+            <Lock />
+          </InputAdornment>
+        }
+      />
+    </FormControl>
+
+    <button> Registration</button>
+  </form> )
+}
+
+export default LoginForm
